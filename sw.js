f2c59e8f0f2a39b8b2db0d66e691fe58c69d95e3
JustinPM <dr.wierd@gmail.com>
2026-01-20 04:40:08 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,26 @@
-const CACHE_NAME = 'voicenotes-v1';
+const CACHE_NAME = 'voicenotes-v2';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json',
-  // Add paths to your icons here, e.g., './icon.png'
+  './manifest.json'
 ];
 
-// Install Event: Caching the app shell
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Activate Event: Cleanup old caches
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(k => caches.delete(k)));
     })
   );
 });
 
-// Fetch Event: Offline-first strategy
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
